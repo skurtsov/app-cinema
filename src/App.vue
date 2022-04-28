97f45dfb60b46251e1c225627e2bf017b8fa9a1d
skurtsov <skurtsov@gmail.com>
2022-04-28 14:02:04 +0300

    bg change on click by card

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,8 @@
         v-for="(idx, key) in films"
         :key="key"
         :style="{ backgroundImage: 'url(' + f_data[key] + ')' }"
-      >
-        >
-      </div>
+        v-on:click="changeFilm(key)"
+      ></div>
     </div>
   </div>
 </template>
@@ -25,11 +24,20 @@
 
 export default {
   name: "App",
-  created() {},
+  created() {
+    this.current_BG = this.all_background[0];
+  },
   data() {
     return {
       films: [1, 2, 3, 4, 5],
-      current_BG: require("@/assets/0.jpg"),
+      current_BG: "",
+      all_background: [
+        require("./assets/0.jpg"),
+        require("./assets/1.jpg"),
+        require("./assets/2.jpg"),
+        require("./assets/3.jpg"),
+        require("./assets/4.jpg"),
+      ],
       f_data: [
         require("./assets/thumbs/0.jpg"),
         require("./assets/thumbs/1.jpg"),
@@ -37,9 +45,12 @@ export default {
         require("./assets/thumbs/3.jpg"),
         require("./assets/thumbs/4.jpg"),
       ],
-      image: "",
-      tmp: [],
     };
+  },
+  methods: {
+    changeFilm(index) {
+      this.current_BG = this.all_background[index];
+    },
   },
 };
 </script>
