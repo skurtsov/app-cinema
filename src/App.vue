2141a61ef024b92561467cd4cf86c8a6bc792273
skurtsov <skurtsov@gmail.com>
2022-04-28 17:23:38 +0300

    JSON data work with

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     :style="{ 'background-image': 'url(' + current_BG + ')' }"
   >
     <div class="name-film">
-      <h1>Film</h1>
+      <h1>{{ current_Title }}</h1>
     </div>
     <div class="cards">
       <div
@@ -19,18 +19,24 @@
 </template>
 
 <script>
-//import data from "../data.json";
+import data from "../data.json";
 //import image from "./assets/thumbs/0.jpg";
 
 export default {
   name: "App",
   created() {
     this.current_BG = this.all_background[0];
+    this.j_films.push(data);
+    for (let i = 0; i < this.f_data.length; i++) {
+      this.j_titles.push(this.j_films[0].films[i].title);
+    }
+    this.current_Title = this.j_titles[0];
   },
   data() {
     return {
       films: [1, 2, 3, 4, 5],
       current_BG: "",
+      current_Title: "",
       all_background: [
         require("./assets/0.jpg"),
         require("./assets/1.jpg"),
@@ -45,11 +51,14 @@ export default {
         require("./assets/thumbs/3.jpg"),
         require("./assets/thumbs/4.jpg"),
       ],
+      j_titles: [],
+      j_films: [],
     };
   },
   methods: {
     changeFilm(index) {
       this.current_BG = this.all_background[index];
+      this.current_Title = this.j_titles[index];
     },
   },
 };
