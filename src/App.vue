ebaffcb59152de3c9977483f37520174d00d291f
skurtsov <skurtsov@gmail.com>
2022-05-02 15:00:45 +0300

    modal window component with trailer

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     class="screen"
     :style="{ 'background-image': 'url(' + current_BG + ')' }"
   >
+    <div class="overlay-bg"></div>
     <div class="name-film">
       <h1>{{ current_Title }}</h1>
     </div>
+
     <div class="cards">
       <div
         class="card"
@@ -13,29 +15,41 @@
         :key="key"
         :style="{ backgroundImage: 'url(' + f_data[key] + ')' }"
         v-on:click="changeFilm(key)"
-      ></div>
+      >
+        <div class="age"></div>
+        <div class="trid"></div>
+      </div>
     </div>
+
+    <ModalWindow :UrlTitle="current_Trailer" />
   </div>
 </template>
 
 <script>
 import data from "../data.json";
 //import image from "./assets/thumbs/0.jpg";
+import ModalWindow from "./components/ModalWindow.vue";
 
 export default {
   name: "App",
+  components: {
+    ModalWindow,
+  },
   created() {
     this.current_BG = this.all_background[0];
     this.j_films.push(data);
     for (let i = 0; i < this.f_data.length; i++) {
       this.j_titles.push(this.j_films[0].films[i].title);
+      this.j_trailers.push(this.j_films[0].films[i].trailer);
     }
     this.current_Title = this.j_titles[0];
   },
   data() {
     return {
+      isModal: false,
       films: [1, 2, 3, 4, 5],
       current_BG: "",
+      current_Trailer: "https://www.youtube.com/embed/s7EdQ4FqbhY",
       current_Title: "",
       all_background: [
         require("./assets/0.jpg"),
@@ -53,12 +67,14 @@ export default {
       ],
       j_titles: [],
       j_films: [],
+      j_trailers: [],
     };
   },
   methods: {
     changeFilm(index) {
       this.current_BG = this.all_background[index];
       this.current_Title = this.j_titles[index];
+      this.current_Trailer = this.j_trailers[index];
     },
   },
 };
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="play" @click="ModalShow = true">
+    <i class="fa-solid fa-circle-play"></i>
+  </div>
+  <div class="modal-backdrop" v-if="ModalShow">
+    <div class="modal-overlay" @click="ModalShow = false"></div>
+    <div class="modal">
+      <header class="modal-header">
+        <button type="button" class="btn-close" @click="ModalShow = false">
+          x
+        </button>
+      </header>
+
+      <section class="modal-body">
+        <slot name="body"
+          ><iframe
+            width="100%"
+            height="98%"
+            :src="UrlTitle"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe
+        ></slot>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalWindow",
+  props: ["UrlTitle"],
+  data() {
+    return {
+      ModalShow: false,
+    };
+  },
+};
+</script>
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #1a1a1a;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+  width: 1000px;
+  height: 700px;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  position: relative;
+  color: #4aae9b;
+  justify-content: space-between;
+}
+
+.modal-footer {
+  border-top: 1px solid #eeeeee;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+  height: 100%;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+.play {
+  font-size: 50px;
+  height: 100px;
+  color: #fff;
+  position: absolute;
+  left: 49%;
+  top: 32%;
+}
+.modal-overlay {
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #0000004d;
+}
+</style>
